dd4c51e0fd05a290398e985f0be37d7253c887ed
jddeguia <justin.dayaodeguia@gmail.com>
2025-03-26 17:46:10 +0800

    feature/ revise base airbyte billing orders

--- key_academy/models/base/base_airbyte_billing_orders.sql
+++ key_academy/models/base/base_airbyte_billing_orders.sql
@@ -4,5 +4,30 @@
     )
 }}
 
-SELECT *
-FROM {{ source('raw_airbyte_billing_orders', 'test_orders_raw__stream_billing_orders') }}
+WITH airbyte_base_data AS (
+  SELECT 
+    _airbyte_raw_id AS airbyte_raw_id,
+    _airbyte_extracted_at AS airbyte_extracted_at,
+    _airbyte_loaded_at AS airbyte_loaded_at,
+    _airbyte_generation_id AS airbyte_generation_id,
+    JSON_QUERY(_airbyte_data, '$')  AS airbyte_data, 
+    JSON_QUERY(_airbyte_meta, '$')  AS airbyte_meta,
+  FROM {{ source('raw_airbyte_billing_orders', 'test_orders_raw__stream_billing_orders') }}
+),
+
+unnest_data AS (
+  SELECT 
+    -- airbyte_raw_id,
+    -- airbyte_extracted_at,
+    -- airbyte_loaded_at,
+    -- airbyte_generation_id,
+    JSON_VALUE(airbyte_data, '$._id') AS id,
+    JSON_VALUE(airbyte_data, '$.id') AS order_id,
+    JSON_VALUE(airbyte_data, '$.status') AS status,
+    JSON_VALUE(airbyte_data, '$.createdAt') AS created_at
+  FROM airbyte_base_data
+  LEFT JOIN UNNEST(JSON_QUERY_ARRAY(airbyte_data, '$.allowedPaymentMethods.paymentMethods')) AS payment_method
+  LEFT JOIN UNNEST(JSON_QUERY_ARRAY(airbyte_data, '$.allowedPaymentMethods.additionalInformation.RiskAssessments')) AS risk_assessment
+)
+
+SELECT * FROM unnest_data
--- key_academy/models/base/base_billing_orders.sql
+++ key_academy/models/base/base_billing_orders.sql
@@ -4,5 +4,23 @@
     )
 }}
 
-SELECT *
-FROM {{ source('raw_test_billing_orders', 'billing_orders') }}
+WITH biling_orders_base AS (
+  SELECT 
+    _airbyte_raw_id AS airbyte_raw_id,
+    _airbyte_extracted_at AS airbyte_extracted_at,
+    _airbyte_generation_id AS airbyte_generation_id,
+    REPLACE(id, 'Order:', '') AS order_id,
+    _id AS id,
+    status AS order_status,
+    createdAt AS created_at,
+    _ab_cdc_cursor AS ab_cdc_cursor,
+    _ab_cdc_updated_at AS ab_cdc_updated_at
+  FROM {{ source('raw_test_billing_orders', 'billing_orders')}}
+),
+
+unnest_columns AS (
+  SELECT * 
+  FROM biling_orders_base
+)
+
+SELECT * FROM unnest_columns
