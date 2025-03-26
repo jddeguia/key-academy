eb38206bf6a9fe90aff8c3d1251070716d2372f0
jddeguia <justin.dayaodeguia@gmail.com>
2025-03-26 18:00:19 +0800

    feature: flatten JSON data from airbyte

--- key_academy/models/base/base_airbyte_billing_orders.sql
+++ key_academy/models/base/base_airbyte_billing_orders.sql
@@ -17,17 +17,23 @@ WITH airbyte_base_data AS (
 
 unnest_data AS (
   SELECT 
-    -- airbyte_raw_id,
-    -- airbyte_extracted_at,
-    -- airbyte_loaded_at,
-    -- airbyte_generation_id,
+    airbyte_raw_id,
+    airbyte_extracted_at,
+    airbyte_loaded_at,
+    airbyte_generation_id,
     JSON_VALUE(airbyte_data, '$._id') AS id,
     JSON_VALUE(airbyte_data, '$.id') AS order_id,
     JSON_VALUE(airbyte_data, '$.status') AS status,
-    JSON_VALUE(airbyte_data, '$.createdAt') AS created_at
+    JSON_VALUE(airbyte_data, '$.createdAt') AS created_at,
+    JSON_VALUE(payment_method, '$.value') AS payment_method,
+    JSON_VALUE(risk_assessment, '$.value') AS risk_assessment,
+    JSON_VALUE(airbyte_data, '$.invoiceData.kind') AS invoice_kind,
+    JSON_VALUE(airbyte_data, '$.deletionInfo.isDeleted') AS is_deleted,
+    JSON_VALUE(airbyte_data, '$.deletionInfo.kind') AS deletion_kind,
+    JSON_VALUE(airbyte_data, '$.purchaser.accountId') AS purchaser_account_id
   FROM airbyte_base_data
-  LEFT JOIN UNNEST(JSON_QUERY_ARRAY(airbyte_data, '$.allowedPaymentMethods.paymentMethods')) AS payment_method
-  LEFT JOIN UNNEST(JSON_QUERY_ARRAY(airbyte_data, '$.allowedPaymentMethods.additionalInformation.RiskAssessments')) AS risk_assessment
+  LEFT JOIN UNNEST(JSON_VALUE_ARRAY(airbyte_data, '$.allowedPaymentMethods.paymentMethods')) AS payment_method
+  LEFT JOIN UNNEST(JSON_VALUE_ARRAY(airbyte_data, '$.allowedPaymentMethods.additionalInformation.RiskAssessments')) AS risk_assessment
 )
 
 SELECT * FROM unnest_data
