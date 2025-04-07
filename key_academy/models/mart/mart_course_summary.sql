cc98ba8fbcfb79fbf19463839b3e3510c2308155
jddeguia <justin.dayaodeguia@gmail.com>
2025-04-08 00:20:15 +0800

    mart model for course progress

--- key_academy/models/mart/mart_course_summary.sql
+++ key_academy/models/mart/mart_course_summary.sql
@@ -4,50 +4,51 @@
     )
 }}
 
- WITH license_unlocked AS (
+WITH learn_tree_published_nodes AS (
     SELECT 
-        user_id,
-        root_node_id AS node_id,
-        license_id,
-        tree_unlocked_state_datetimestamp AS course_purchased_at
-    FROM {{ ref('mart_learn_tree_unlocked_states')}}   
- ),
+        node_id AS root_node_id,
+        title
+    FROM {{ ref('mart_trees_published_nodes')}}    
+    WHERE structure_type = 'Root'
+),
 
- license_trial_started AS (
-    SELECT 
-        user_id,
-        started_at AS trial_started_at,
-        root_node_id AS node_id,
-        status
-    FROM {{ ref('mart_learn_tree_states')}}  
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY user_id, node_id ORDER BY started_at ASC) = 1
- ),
-
- license_definitions AS (
+learn_tree_states AS (
     SELECT
-        id,
-        license_definition_id,
-        node_id,
-        license_name AS title
-    FROM {{ ref('mart_license_definitions')}}  
-    WHERE license_name NOT LIKE '%InApp%'
- ),
+        started_at,
+        user_id,
+        root_node_id,
+        head_content_node_id,
+        status,
+    FROM {{ ref('mart_learn_tree_states')}}    
+),
 
-course_summary AS (
+learn_tree_unlocked_states AS (
     SELECT
-        lu.user_id,
-        lu.license_id,
-        lu.node_id,
-        ld.title,
-        lu.course_purchased_at,
-        ls.trial_started_at,
-    FROM license_unlocked lu
-    LEFT JOIN license_trial_started ls USING (user_id, node_id)
-    LEFT JOIN license_definitions ld USING (node_id)
- )
+        tree_unlocked_state_datetimestamp,
+        user_id,
+        root_node_id,
+        learn_tree_unlocked_kind,
+        license_id
+    FROM {{ ref('mart_learn_tree_unlocked_states')}}    
+),
 
-SELECT *
-FROM course_summary
+summary AS (
+  SELECT
+    ts.user_id,
+    ts.root_node_id,
+    p.title AS course_title,
+    ts.head_content_node_id AS current_node_id,
+    ts.started_at AS module_started_at,
+    ts.status AS module_progress_status,
+    us.tree_unlocked_state_datetimestamp AS module_unlocked_datetimestamp,
+    us.learn_tree_unlocked_kind,
+    us.license_id,
+  FROM learn_tree_states ts
+  LEFT JOIN learn_tree_unlocked_states us USING (user_id, root_node_id)
+  LEFT JOIN learn_tree_published_nodes p USING (root_node_id )
+)
+
+SELECT * FROM summary
 
 
 
