cbc9de89b3127eadf177625d560a51804bda10b9
jddeguia <justin.dayaodeguia@gmail.com>
2025-04-15 00:25:41 +0800

    qck fix

--- key_academy/models/mart/core/mart_agg_user_progress.sql
+++ key_academy/models/mart/core/mart_agg_user_progress.sql
@@ -10,6 +10,7 @@ SELECT
     SUM(total_modules) AS total_modules_available,
     SUM(modules_started) AS total_modules_started,
     SUM(modules_finished) AS total_modules_finished,
+    AVG(performance_in_percent) AS average_performance_in_percent,
     SAFE_DIVIDE(SUM(modules_finished), SUM(total_modules)) AS overall_completion_rate,
     COUNTIF(certificate_id IS NOT NULL) AS total_certificates_earned,
     COUNTIF(LOWER(is_with_honors) = 'true') AS total_certificates_with_honors,
