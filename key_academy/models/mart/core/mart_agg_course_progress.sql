a68ca2d49b9d7b4299e019d784179df81fe87f9e
jddeguia <justin.dayaodeguia@gmail.com>
2025-04-24 23:50:51 +0800

    revise agg course progresS

--- key_academy/models/mart/core/mart_agg_course_progress.sql
+++ key_academy/models/mart/core/mart_agg_course_progress.sql
@@ -14,7 +14,9 @@ kpis AS (
         DATE(module_started_at) AS module_started_date,
         course_title,
         SUM(CASE WHEN module_started_at IS NOT NULL AND module_unlocked_at IS NULL THEN 1 ELSE 0 END) AS trials,
+        COUNTIF(module_progress_status = 'Started') AS courses_started,
         COUNTIF(module_progress_status = 'Finished') AS lessons_completed,
+        SUM(CASE WHEN certificate_id IS NOT NULL THEN 1 ELSE 0 END) AS certificates,
         SUM(CASE WHEN license_id IS NOT NULL THEN 1 ELSE 0 END) AS licenses,
         SUM(net_price) AS revenue
     FROM module_progress
