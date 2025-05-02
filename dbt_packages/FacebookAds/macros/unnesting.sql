24189fcabcff2c8f4fd89394b3b424cee501213b
jddeguia <justin.dayaodeguia@gmail.com>
2025-05-02 23:36:42 +0800

    delete

--- dbt_packages/FacebookAds/macros/unnesting.sql
+++
@@ -1,9 +0,0 @@
-{% macro unnesting(variable) %}
-
-    {% if target.type =='snowflake' %}
-    , LATERAL FLATTEN( input => PARSE_JSON({{variable}})) {{variable}}
-    {% else %}
-    left join unnest({{variable}}) {{variable}}
-    {% endif %}
-    
-{% endmacro %}
