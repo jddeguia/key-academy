6115b24fe4ea4411bd5238d5147aa82b4b13843d
jddeguia <justin.dayaodeguia@gmail.com>
2025-05-07 00:04:30 +0800

    delete ununsed models staging

--- models/staging/ga4_events/stg_ga4_app_and_device_events.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized='table',
-    partition_by={'field': 'event_timestamp', 'data_type': 'timestamp'},
-    cluster_by=['event_name', 'user_id', 'device_category']
-) }}
-
-SELECT *
-FROM {{ ref('base_ga4_events') }}
-WHERE event_name IN ('app_remove', 'app_update', 'os_update')
