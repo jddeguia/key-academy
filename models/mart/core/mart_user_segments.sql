d12c75046d21b0b42b9c17d25f2d4a798fd15271
jddeguia <justin.dayaodeguia@gmail.com>
2025-05-13 22:38:06 +0800

    update age bucket

--- models/mart/core/mart_user_segments.sql
+++ models/mart/core/mart_user_segments.sql
@@ -13,8 +13,7 @@ WITH user_info AS (
             WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) < 29 THEN '<29'
             WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) BETWEEN 30 AND 39 THEN '30-39'
             WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) BETWEEN 40 AND 49 THEN '40-49'
-            WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) BETWEEN 50 AND 59 THEN '50-59'
-            WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) >= 60 THEN '>59'
+            WHEN DATE_DIFF(CURRENT_DATE(), DATE(customer_date_of_birth), YEAR) >= 50 THEN '50+'
             ELSE NULL
         END AS age_bucket
     FROM {{ ref('base_billing_orders') }}
